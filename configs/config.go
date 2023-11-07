06be70f61bddd82845a6a5a50c3edb88c8a52f64
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-11-07 21:56:11 +0700

    add: initial project structure

--- configs/config.go
+++ configs/config.go
@@ -1,1 +1,29 @@
 package configs
+
+import (
+	"log"
+	"os"
+)
+
+type Configuration struct {
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_PORT     string
+	DB_HOST     string
+	DB_NAME     string
+}
+
+func GetConfig() Configuration {
+
+	configuration := Configuration{
+		DB_USERNAME: os.Getenv("DB_USERNAME"),
+		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
+		DB_PORT:     os.Getenv("DB_PORT"),
+		DB_HOST:     os.Getenv("DB_HOST"),
+		DB_NAME:     os.Getenv("DB_NAME"),
+	}
+
+	log.Print(configuration)
+
+	return configuration
+}
