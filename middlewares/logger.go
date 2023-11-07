06b4c94373bbdffa4340b10c1f94693c1f83453c
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-11-07 21:46:19 +0700

    feat: add middlewares

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -1,1 +1,12 @@
 package middlewares
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Logger(e *echo.Echo) {
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}, latency=${latency_human}\n",
+	}))
+}
