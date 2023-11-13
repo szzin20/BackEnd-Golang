cac93ff7ba63c98a1366a4a9df3c70d1c2597c07
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-11-13 18:29:46 +0700

    feat: remove doctor(controllers, models, request, response)

--- models/web/doctor-request.go
+++ models/web/doctor-request.go
@@ -1,28 +1,2 @@
 package web
 
-type DoctorRegisterRequest struct {
-	Name               string `json:"name" form:"name"`
-	Email              string `json:"email" form:"email"`
-	Password           string `json:"password" form:"password"`
-	Status             bool   `json:"status" form:"status"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ImageURL           string `json:"image_url" form:"image_url"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
-}
-
-type DoctorLoginRequest struct {
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-}
-
-type DoctorUpdateRequest struct {
-	Name               string `json:"name" form:"name"`
-	Email              string `json:"email" form:"email"`
-	Password           string `json:"password" form:"password"`
-	Status             bool   `json:"status" form:"status"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ImageURL           string `json:"image_url" form:"image_url"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
-}
--- models/web/doctor-response.go
+++ models/web/doctor-response.go
@@ -1,29 +1,2 @@
 package web
 
-type DoctorResgisterResponse struct {
-	Name               string `json:"name" form:"name"`
-	Email              string `json:"email" form:"email"`
-	Password           string `json:"password" form:"password"`
-	Status             bool   `json:"status" form:"status"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ImageURL           string `json:"image_url" form:"image_url"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
-}
-
-type DoctorLoginResponse struct {
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-	Token    string `json:"token" form:"token"`
-}
-
-type DoctorUpdateResponse struct {
-	Name               string `json:"name" form:"name"`
-	Email              string `json:"email" form:"email"`
-	Password           string `json:"password" form:"password"`
-	Status             bool   `json:"status" form:"status"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ImageURL           string `json:"image_url" form:"image_url"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
-}
