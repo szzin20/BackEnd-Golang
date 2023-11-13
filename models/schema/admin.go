e2ba63a868b9e61685c9e2f3bb8c53ebceb655d6
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-11-13 22:23:19 +0700

    feat: add admin models(schema, web)

--- models/schema/admin.go
+++ models/schema/admin.go
@@ -1,2 +1,17 @@
 package schema
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Admin struct {
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
--- models/web/admin-request.go
+++ models/web/admin-request.go
@@ -1,1 +1,18 @@
 package web
+
+type AdminLoginRequest struct {
+	Email    string `json:"email" form:"email" `
+	Password string `json:"password" form:"password" `
+}
+
+type AdminUpdateRequest struct {
+	Name     string `json:"name" form:"name" `
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password" `
+}
+
+type AdminResetPasswordRequest struct {
+	Email              string `json:"email" form:"email" `
+	NewPassword        string `json:"newPassword" form:"password" `
+	ConfirmNewPassword string `json:"confirmNewPassword" form:"confirmNewPassword" `
+}
