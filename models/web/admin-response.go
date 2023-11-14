05374c8011ddbb15872c519f4b10f84992def9cf
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-11-14 23:35:02 +0700

    feat: update web(admin-response)

--- models/web/admin-response.go
+++ models/web/admin-response.go
@@ -3,11 +3,8 @@ package web
 type AdminLoginResponse struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Token    string `json:"token"`
 }
 type AdminUpdateResponse struct {
-	Id       uint   `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Password string `json:"password"`
 }
