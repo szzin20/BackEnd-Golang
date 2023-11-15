baf0859bd1e8488c70171cc4d589b23950ca639a
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-11-15 22:44:38 +0700

    feat: update middlewares jwt

---
+++ middlewares/jwt-auth-user.go
@@ -0,0 +1,63 @@
+package middlewares
+
+import (
+	"healthcare/utils/helper"
+	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+)
+
+// authentication and authorization USER
+const role = "user"
+
+// check "role" only
+func UserRoleAuth(next echo.HandlerFunc) echo.HandlerFunc {
+
+	return func(c echo.Context) error {
+		token, err := ExtractToken(c)
+		if err != nil {
+			return err
+		}
+
+		if token.Valid {
+			userClaims := token.Claims.(jwt.MapClaims)
+			userRole := userClaims["role"].(string)
+
+			if userRole == role {
+				return next(c)
+			} else {
+				return c.JSON(http.StatusForbidden, helper.ErrorResponse("You are not Authorized to Access this Resource"))
+			}
+		} else {
+			return c.JSON(http.StatusUnauthorized, helper.ErrorResponse("Invalid or Expired Token"))
+		}
+	}
+}
+
+// check "role" and "id"
+func UserIDRoleAuth(next echo.HandlerFunc) echo.HandlerFunc {
+
+	return func(c echo.Context) error {
+		token, err := ExtractToken(c)
+		if err != nil {
+			return err
+		}
+
+		if token.Valid {
+			userClaims := token.Claims.(jwt.MapClaims)
+			userRole := userClaims["role"].(string)
+			userID := int(userClaims["id"].(float64))
+
+			if userRole == role {
+				c.Set("userID", userID)
+				return next(c)
+			} else {
+				return c.JSON(http.StatusForbidden, helper.ErrorResponse("You are not Authorized to Access this Resource"))
+			}
+		} else {
+			return c.JSON(http.StatusUnauthorized, helper.ErrorResponse("Invalid or Expired Token"))
+			
+		}
+	}
+}
--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -1,27 +1,45 @@
 package middlewares
 
 import (
-	"healthcare/models/web"
+	"healthcare/utils/helper"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
 )
 
-func GenerateToken(userLoginResponse *web.UserLoginResponse, id int) (string, error) {
-
-	claims := jwt.MapClaims{}
-	claims["authorized"] = true
-	claims["id"] = id
-	claims["name"] = userLoginResponse.Fullname
-	claims["email"] = userLoginResponse.Email
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	validToken, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+// create token
+func GenerateToken(userID uint, email string, role string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["id"] = userID
+	claims["email"] = email
+	claims["role"] = role
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix() // 3 hari
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 	if err != nil {
 		return "", err
 	}
+	return tokenString, nil
+}
 
-	return validToken, nil
+func ExtractToken(c echo.Context) (*jwt.Token, error) {
+	tokenString := c.Request().Header.Get("Authorization")
+	if tokenString == "" {
+		return nil, c.JSON(http.StatusUnauthorized, helper.ErrorResponse("Missing Token"))
+	}
+
+	tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil {
+		return nil, c.JSON(http.StatusUnauthorized, helper.ErrorResponse("Invalid or Expired Token"))
+	}
+
+	return token, nil
 }
