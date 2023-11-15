e491f26f276845fb66133ea6812e07f8ecfb414a
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-11-15 10:22:31 +0700

    feat: add Email

--- utils/helper/email.go
+++ utils/helper/email.go
@@ -1,56 +1,128 @@
 package helper
 
 import (
+	"errors"
+	"fmt"
+	"log"
 	"os"
 	"strconv"
 
 	"gopkg.in/gomail.v2"
 )
 
-// sendEmail sends an email
-func sendEmail(to, subject, body string) error {
-	// Set up email server configuration
+func SendEmail(to, subject, content string) error {
+	// Konfigurasi server email
 	smtpServer := os.Getenv("SMTPSERVER")
 	smtpPortStr := os.Getenv("SMTPPORT")
 	smtpUsername := os.Getenv("SMTPUSERNAME")
 	smtpPassword := os.Getenv("SMTPPASSWORD")
 
-	// Convert smtpPortStr to int
+	// Periksa apakah semua variabel lingkungan
+	if smtpServer == "" || smtpPortStr == "" || smtpUsername == "" || smtpPassword == "" {
+		return errors.New("Konfigurasi SMTP belum lengkap. Mohon atur semua variabel lingkungan yang diperlukan.")
+	}
+
+	// Konversi smtpPortStr ke int
 	smtpPort, err := strconv.Atoi(smtpPortStr)
 	if err != nil {
+		log.Println("Gagal mengonversi port SMTP ke integer:", err)
 		return err
 	}
 
-	// Create a new email message
+	// Buat pesan email baru
 	m := gomail.NewMessage()
 	m.SetHeader("From", smtpUsername)
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", subject)
-	m.SetBody("text/plain", body)
 
-	// Set up the email server dialer
+	// Tambahkan bagian HTML ke body email
+	htmlBody := fmt.Sprintf(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<style>
+				/* CSS */
+				body {
+					font-family: Arial, sans-serif;
+					background-color: #f4f4f4;
+					padding: 20px;
+				}
+				.container {
+					background-color: #ffffff;
+					padding: 20px;
+					border-radius: 10px;
+				}
+				h1 {
+					color: #007bff;
+				}
+				p {
+					color: #333;
+				}
+			</style>
+		</head>
+		<body>
+			<div class="container">
+				<h1>%s</h1>
+				<p>%s</p>
+			</div>
+		</body>
+		</html>
+	`, subject, content)
+
+	m.SetBody("text/html", htmlBody)
+
+	// Konfigurasi dialer untuk server email
 	d := gomail.NewDialer(smtpServer, smtpPort, smtpUsername, smtpPassword)
 
-	// Send the email
+	// Kirim email
 	if err := d.DialAndSend(m); err != nil {
+		log.Println("Gagal mengirim email:", err)
 		return err
 	}
+
+	log.Printf("Email berhasil dikirim ke %s\n", to)
 
 	return nil
 }
 
-// sendVerificationEmail sends a verification email
-func SendVerificationEmail(to, verificationCode string) error {
-	subject := "Email Verification"
-	body := "Your verification code is: " + verificationCode
+// SendNotificationEmail mengirimkan email notifikasi
+// SendNotificationEmail sends a notification email based on the user type
+func SendNotificationEmail(to, fullname, notificationType, userType string) error {
+	go func() {
+		var subject, body string
 
-	return sendEmail(to, subject, body)
-}
+		switch notificationType {
+		case "login":
+			subject = "Login Notification"
+			body = "Hello, "
 
-// sendLoginNotificationEmail sends a login notification email
-func SendLoginNotificationEmail(to, username, body string ) error {
-	subject := "Login Notification"
-	body = "Hello, " + username + "! You have successfully logged in."
+			// Customize the greeting based on the user type
+			if userType == "drg" {
+				body += "Drg. "
+			}
 
-	return sendEmail(to, subject, body)
+			body += fullname + "! Anda telah berhasil masuk."
+		case "register":
+			subject = "Registration Notification"
+			body = "Hello, "
+
+			// Customize the greeting based on the user type
+			if userType == "drg" {
+				body += "Drg. "
+			}
+
+			body += fullname + "! Anda telah berhasil mendaftar."
+		default:
+			err := errors.New("Jenis notifikasi tidak valid")
+			log.Println(err)
+			return
+		}
+
+		err := SendEmail(to, subject, body)
+		if err != nil {
+			log.Println("Gagal mengirim email:", err)
+		}
+	}()
+
+	return nil
 }
--- utils/helper/generate-verification-code.go
+++ utils/helper/generate-verification-code.go
@@ -2,13 +2,40 @@ package helper
 
 import (
 	"crypto/rand"
-	"encoding/base64"
+	"math/big"
 )
 
+// GenerateVerificationCode generates a random alphanumeric verification code.
 func GenerateVerificationCode() string {
-	bytes := make([]byte, 32)
-	if _, err := rand.Read(bytes); err != nil {
-		panic(err)
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	codeLength := 8
+	randomBytes := make([]byte, codeLength)
+	for i := range randomBytes {
+		randIndex, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			panic(err)
+		}
+		randomBytes[i] = charset[randIndex.Int64()]
 	}
-	return base64.URLEncoding.EncodeToString(bytes)
+
+	return string(randomBytes)
 }
+
+// // GetDoctorOTPByEmail mengambil OTP dari database berdasarkan email dokter
+// func GetDoctorOTPByEmail(email string) (string, error) {
+// 	var doctor schema.Doctor
+// 	if err := configs.DB.Where("email = ?", email).First(&doctor).Error; err != nil {
+// 		return "", err
+// 	}
+
+// 	return doctor.OTP, nil
+// }
+
+// // VerifyDoctorOTP memverifikasi OTP untuk email yang diberikan dan dokter
+// func VerifyDoctorOTP(email, enteredOTP string) bool {
+// 	storedOTP, err := GetDoctorOTPByEmail(email)
+// 	if err != nil {
+// 		return false
+// 	}
+// 	return enteredOTP == storedOTP
+// }
