c1063dd0a14d97c44da75be8d2ab68aee1f1b7cc
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-11-15 10:15:13 +0700

    feat: update schema(doctor)

--- models/schema/doctor.go
+++ models/schema/doctor.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Doctor struct {
-	ID                 int    `gorm:"primarykey"`
+	ID                 uint   `gorm:"primarykey"`
 	Fullname           string `gorm:"not null"`
 	Email              string `gorm:"not null;unique"`
 	Password           string `gorm:"not null"`
-	Status             bool   
+	Status             string `gorm:"not null"`
 	Price              int    `gorm:"not null"`
 	Tag                string `gorm:"not null"`
-	ImageURL           string `gorm:"not null"`
+	ProfilePicture     string `gorm:"not null"`
 	RegistrationLetter string `gorm:"not null"`
 	Role               string `gorm:"type:enum('doctor');default:'doctor'"`
 	UpdatedAt          time.Time
