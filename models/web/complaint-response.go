2b4bb85ca63c66ae2c6e0a349ef4424f4e9ec01b
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-11-15 10:17:46 +0700

    feat: add  web(doctor-complaintResponse)

--- models/web/complaint-response.go
+++ models/web/complaint-response.go
@@ -1,2 +1,8 @@
 package web
 
+type ComplaintsUpdateResponse struct {
+	DoctorTransactionID uint   `json:"doctor_transactionID" form:"doctor_transactionID"`
+	Title               string `json:"title" form:"title"`
+	Content             string `json:"content" form:"content"`
+	Status              bool   `json:"status" form:"status"`
+}
