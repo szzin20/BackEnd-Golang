b700b31167fe7778852b569d2568d22dd29c72c7
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-11-16 22:30:13 +0700

    feat: add endpoint route

--- routes/route.go
+++ routes/route.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"healthcare/controllers"
+
 	"github.com/labstack/echo/v4"
 )
 
 func SetupRoutes(e *echo.Echo) {
+
+	e.POST("/users/create/doctor-transaction", controllers.CreateDoctorTransaction)
 
 }
