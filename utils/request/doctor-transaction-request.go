6afeb6fbfe0eeaf7c2e995d18a40a4ccdcbff121
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-11-16 22:26:16 +0700

    feat: add utils(request, response)

--- utils/request/doctor-transaction-request.go
+++ utils/request/doctor-transaction-request.go
@@ -1,1 +1,17 @@
 package request
+
+import (
+	"healthcare/models/schema"
+	"healthcare/models/web"
+)
+
+func ConvertToCreateDTRequest(DoctorTransaction web.CreateDoctorTransactionRequest) *schema.DoctorTransaction {
+	return &schema.DoctorTransaction{
+		DoctorID:      DoctorTransaction.DoctorID,
+		UserID:        DoctorTransaction.UserID,
+		HealthDetails: DoctorTransaction.HealthDetails,
+		PaymentMethod: DoctorTransaction.PaymentMethod,
+		Price:         DoctorTransaction.Price,
+		ImageURL:      DoctorTransaction.ImageURL,
+	}
+}
--- utils/response/doctor-transaction-response.go
+++ utils/response/doctor-transaction-response.go
@@ -1,2 +1,35 @@
 package response
 
+import (
+	"healthcare/models/schema"
+	"healthcare/models/web"
+)
+
+func ConvertToCreateDTResponse(DoctorTransaction *schema.DoctorTransaction) web.CreateDoctorTransactionResponse {
+	return web.CreateDoctorTransactionResponse{
+		DoctorID:      DoctorTransaction.DoctorID,
+		UserID:        DoctorTransaction.UserID,
+		HealthDetails: DoctorTransaction.HealthDetails,
+		PaymentMethod: DoctorTransaction.PaymentMethod,
+		Price:         DoctorTransaction.Price,
+		ImageURL:      DoctorTransaction.ImageURL,
+		PaymentStatus: DoctorTransaction.PaymentStatus,
+	}
+}
+
+func ConvertToGetAllDTResponse(doctorTransactions []schema.DoctorTransaction) []web.CreateDoctorTransactionResponse {
+	var results []web.CreateDoctorTransactionResponse
+	for _, dt := range doctorTransactions {
+		dtResponse := web.CreateDoctorTransactionResponse{
+			DoctorID:      dt.DoctorID,
+			UserID:        dt.UserID,
+			HealthDetails: dt.HealthDetails,
+			PaymentMethod: dt.PaymentMethod,
+			Price:         dt.Price,
+			ImageURL:      dt.ImageURL,
+			PaymentStatus: dt.PaymentStatus,
+		}
+		results = append(results, dtResponse)
+	}
+	return results
+}
