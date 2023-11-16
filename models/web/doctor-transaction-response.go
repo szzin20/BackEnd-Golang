3d680a52e1545f35bf5b710a77643192d22b3300
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-11-16 22:24:53 +0700

    feat: add doctor-transaction models(web, schema)

--- models/web/doctor-transaction-response.go
+++ models/web/doctor-transaction-response.go
@@ -1,2 +1,11 @@
 package web
 
+type CreateDoctorTransactionResponse struct {
+	DoctorID      uint   `json:"doctor_id" form:"doctor_id"`
+	UserID        uint   `json:"user_id" form:"user_id"`
+	HealthDetails string `json:"health_details" form:"health_details"`
+	PaymentMethod string `json:"payment_method" form:"payment_method"`
+	Price         int    `json:"price" form:"price"`
+	ImageURL      string `json:"image_url" form:"image_url"`
+	PaymentStatus string `json:"payment_status" form:"payment_status"`
+}
