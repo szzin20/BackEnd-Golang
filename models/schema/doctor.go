a4c4a75f3831396382081e4153414f7fea2640ba
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-11-17 19:55:24 +0700

    feat: update field doctors(schema)

--- models/schema/doctor.go
+++ models/schema/doctor.go
@@ -6,18 +6,26 @@ import (
 	"gorm.io/gorm"
 )
 
+type DoctorStatus string
+
+const (
+	Online  DoctorStatus = "Online"
+	Offline DoctorStatus = "Offline"
+)
+
 type Doctor struct {
-	ID                 uint   `gorm:"primarykey"`
-	Fullname           string `gorm:"not null"`
-	Email              string `gorm:"not null;unique"`
-	Password           string `gorm:"not null"`
-	Status             string `gorm:"not null"`
-	Price              int    `gorm:"not null"`
-	Tag                string `gorm:"not null"`
-	ProfilePicture     string `gorm:"not null"`
-	RegistrationLetter string `gorm:"not null"`
-	Role               string `gorm:"type:enum('doctor');default:'doctor'"`
-	UpdatedAt          time.Time
-	CreatedAt          time.Time
-	DeletedAt          gorm.DeletedAt `gorm:"index"`
+	ID                      uint                `gorm:"primarykey"`
+	Fullname                string              `gorm:"not null"`
+	Email                   string              `gorm:"not null;unique"`
+	Password                string              `gorm:"not null"`
+	Status                  DoctorStatus        `gorm:"type:enum('Online', 'Offline')"`
+	Price                   int                 `gorm:"not null"`
+	Tag                     string              `gorm:"not null"`
+	ProfilePicture          string              `gorm:"not null"`
+	RegistrationCertificate int                 `gorm:"not null"`
+	Role                    string              `gorm:"type:enum('doctor');default:'doctor'"`
+	DoctorTransaction       []DoctorTransaction `gorm:"ForeignKey:DoctorID;references:ID"`
+	UpdatedAt               time.Time
+	CreatedAt               time.Time
+	DeletedAt               gorm.DeletedAt `gorm:"index"`
 }
