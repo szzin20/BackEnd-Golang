f3a56b20f4f10ec851e1298a40fac129996da92d
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-11-17 19:56:28 +0700

    feat: update doctors(response)

--- models/web/doctor-response.go
+++ models/web/doctor-response.go
@@ -1,13 +1,15 @@
 package web
 
-type DoctorResgisterResponse struct {
-	Fullname           string `json:"fullname" form:"fullname"`
-	Email              string `json:"email" form:"email"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ProfilePicture     string `json:"profile_picture" form:"profile_picture"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
-}
+import "healthcare/models/schema"
+
+// type DoctorResgisterResponse struct {
+// 	Fullname           string `json:"fullname" form:"fullname"`
+// 	Email              string `json:"email" form:"email"`
+// 	Price              int    `json:"price" form:"price"`
+// 	Tag                string `json:"tag" form:"tag"`
+// 	ProfilePicture     string `json:"profile_picture" form:"profile_picture"`
+// 	RegistrationLetter int    `json:"registration_letter" form:"registration_letter"`
+// }
 
 type DoctorLoginResponse struct {
 	Fullname string `json:"fullname" form:"fullname"`
@@ -15,21 +17,46 @@ type DoctorLoginResponse struct {
 	Token    string `json:"token" form:"token"`
 }
 
+type DoctorLogOutResponse struct {
+	Fullname string `json:"fullname" form:"fullname"`
+	Email    string `json:"email" form:"email"`
+	Tag      string `json:"tag" form:"tag"`
+}
+
 type DoctorUpdateResponse struct {
-	Fullname           string `json:"fullname" form:"fullname"`
-	Email              string `json:"email" form:"email"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ProfilePicture     string `json:"profile_picture" form:"profile_picture"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
+	Fullname                string `json:"fullname" form:"fullname"`
+	Email                   string `json:"email" form:"email"`
+	Price                   int    `json:"price" form:"price"`
+	Tag                     string `json:"tag" form:"tag"`
+	ProfilePicture          string `json:"profile_picture" form:"profile_picture"`
+	RegistrationCertificate int    `json:"registration_certificate" form:"registration_certificate"`
 }
 
 type DoctorAllResponse struct {
-	Fullname           string `json:"fullname" form:"fullname"`
-	Email              string `json:"email" form:"email"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	Status             string `json:"status" form:"status"`
-	ProfilePicture     string `json:"profile_picture" form:"profile_picture"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
+	Fullname                string              `json:"fullname" form:"fullname"`
+	Email                   string              `json:"email" form:"email"`
+	Price                   int                 `json:"price" form:"price"`
+	Tag                     string              `json:"tag" form:"tag"`
+	Status                  schema.DoctorStatus `json:"status" form:"status"`
+	ProfilePicture          string              `json:"profile_picture" form:"profile_picture"`
+	RegistrationCertificate int                 `json:"registration_certificate" form:"registration_certificate"`
 }
+
+// type DoctorPatientsResponse struct {
+// 	DoctorID       uint                  `json:"doctor_id"`
+// 	TransactionID  uint                  `json:"transaction_id"`
+// 	DoctorFullname string                `json:"doctor_fullname"`
+// 	Patients       []PatientInfoResponse `json:"patients"`
+// }
+
+// type PatientInfoResponse struct {
+// 	ID        uint   `json:"id"`
+// 	Name      string `json:"name"`
+// 	Email     string `json:"email"`
+// 	Gender    string `json:"gender"`
+// 	Status    string `json:"status"`
+// 	Height    int    `json:"height"`
+// 	Weight    int    `json:"weight"`
+// 	Birthdate string `json:"birthdate"`
+// 	BloodType string `json:"blood_type"`
+// }
