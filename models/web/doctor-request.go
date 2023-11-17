e0b96d7c26bf0b5a77c66faa78bca366fda7fefd
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-11-17 19:56:51 +0700

    feat: update doctors(request)

--- models/web/doctor-request.go
+++ models/web/doctor-request.go
@@ -1,26 +1,26 @@
 package web
 
-type DoctorRegisterRequest struct {
-	Fullname           string `json:"fullname" form:"fullname"`
-	Email              string `json:"email" form:"email"`
-	Password           string `json:"password" form:"password"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ProfilePicture     string `json:"profile_picture" form:"profile_picture"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
-}
+// type DoctorRegisterRequest struct {
+// 	Fullname                string `json:"fullname" form:"fullname" validate:"required"`
+// 	Email                   string `json:"email" form:"email" validate:"required,email"`
+// 	Password                string `json:"password" form:"password" validate:"required,min=8"`
+// 	Price                   int    `json:"price" form:"price" validate:"required,min=0"`
+// 	Tag                     string `json:"tag" form:"tag" validate:"required"`
+// 	ProfilePicture          string `json:"profile_picture" form:"profile_picture" validate:"url"`
+// 	RegistrationCertificate int    `json:"registration_certificate" form:"registration_certificate" Validate:"required`
+// }
 
 type DoctorLoginRequest struct {
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
+	Email    string `json:"email" form:"email" validate:"required,email"`
+	Password string `json:"password" form:"password" validate:"required,min=8"`
 }
 
 type DoctorUpdateRequest struct {
-	Fullname           string `json:"fullname" form:"fullname"`
-	Email              string `json:"email" form:"email"`
-	Password           string `json:"password" form:"password"`
-	Price              int    `json:"price" form:"price"`
-	Tag                string `json:"tag" form:"tag"`
-	ProfilePicture     string `json:"profile_picture" form:"profile_picture"`
-	RegistrationLetter string `json:"registration_letter" form:"registration_letter"`
+	Fullname                string `json:"fullname" form:"fullname" validate:"required"`
+	Email                   string `json:"email" form:"email" validate:"required,email"`
+	Password                string `json:"password" form:"password" validate:"min=8"`
+	Price                   int    `json:"price" form:"price" validate:"min=0"`
+	Tag                     string `json:"tag" form:"tag" validate:"required"`
+	ProfilePicture          string `json:"profile_picture" form:"profile_picture" validate:"url"`
+	RegistrationCertificate int    `json:"registration_certificate" form:"registration_certificate" Validate:"required"`
 }
