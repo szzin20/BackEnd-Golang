a4f9967ad9b0c89b916f61311dc730c81620d21d
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-11-23 22:12:51 +0700

    feat: update schema doctor transactions

--- models/schema/doctor-transaction.go
+++ models/schema/doctor-transaction.go
@@ -1,25 +1,23 @@
 package schema
 
 import (
-	// "time"
+	"time"
 
 	"gorm.io/gorm"
 )
 
 type DoctorTransaction struct {
-	// ID                  uint   `gorm:"primaryKey"`
-	gorm.Model
+	ID                  uint   `gorm:"primaryKey"`
 	DoctorID            uint   `gorm:"foreignKey:DoctorID"`
 	UserID              uint   `gorm:"foreignKey:UserID"`
 	HealthDetails       string `gorm:"not null"`
-	PaymentMethod       string `gorm:"type:enum('manual', 'gateway')"`
+	ConsultationTime    string `gorm:"type:enum('30 menit');default:'30 menit'"`
 	Price               int    `gorm:"not null"`
+	PaymentMethod       string `gorm:"type:enum('manual transfer bca', 'manual transfer bri', 'manual transfer bni')"`
 	PaymentConfirmation string `gorm:"not null"`
 	PaymentStatus       string `gorm:"type:enum('pending', 'success', 'cancelled');default:'pending'"`
 	PatientStatus       string `gorm:"type:enum('pending', 'solved', 'unsolved');default:'pending'"`
-	// CreatedAt           time.Time
-	// UpdatedAt           time.Time
-	// DeletedAt           *gorm.DeletedAt `gorm:"index"`
-	Doctor              Doctor          //`gorm:"embedded"`
-	// Complaint     Complaint `gorm:"ForeignKey:TransactionID;references:ID"` // one to one
+	CreatedAt           time.Time
+	UpdatedAt           time.Time
+	DeletedAt           *gorm.DeletedAt `gorm:"index"`
 }
