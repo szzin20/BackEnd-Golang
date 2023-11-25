e0f7262eb55030d2098ac11c7bbdd73583aec53d
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-11-26 00:55:20 +0700

    feat: update response doctor transactions

--- models/web/doctor-transaction-response.go
+++ models/web/doctor-transaction-response.go
@@ -3,17 +3,17 @@ package web
 import "time"
 
 type CreateDoctorTransactionResponse struct {
-	ID                  uint      `json:"id"  form:"id"`
-	Fullname            string    `json:"fullname"  form:"fullname"`
-	Specialist          string    `json:"specialist" form:"specialist"`
-	Price               int       `json:"price" form:"price"`
-	PaymentMethod       string    `json:"payment_method" form:"payment_method"`
-	PaymentStatus       string    `json:"payment_status" form:"payment_status"`
-	PaymentConfirmation string    `json:"payment_confirmation" form:"payment_confirmation"`
-	CreatedAt           time.Time `json:"created_at" form:"created_at"`
+	ID                  uint      `json:"id"`
+	Fullname            string    `json:"fullname"`
+	Specialist          string    `json:"specialist"`
+	Price               int       `json:"price"`
+	PaymentMethod       string    `json:"payment_method"`
+	PaymentStatus       string    `json:"payment_status"`
+	PaymentConfirmation string    `json:"payment_confirmation"`
+	CreatedAt           time.Time `json:"created_at"`
 }
 
 type DoctorTransactionsResponse struct {
-	ID                  uint      `json:"id"  form:"id"`
-	Fullname            string    `json:"fullname"  form:"fullname"`
+	ID       uint   `json:"id"`
+	Fullname string `json:"fullname"`
 }
