d208a0e620f51c19c1c23b3a66b61af8808bb778
Mutiakhrnniza2002 <117338908+Mutiakhoirunniza@users.noreply.github.com>
2023-11-25 20:15:57 +0700

    feat: update doctor complaint( env example, controllers, models, utils)

--- models/web/complaint-response.go
+++ models/web/complaint-response.go
@@ -5,31 +5,31 @@ import (
 )
 
 type ComplaintResponse struct {
-	Message       string    `json:"message" form:"message"`
-	Image         string    `json:"image" form:"image"`
-	Audio         string    `json:"audio" form:"audio"`
-	CreatedAt     time.Time `json:"created_at" form:"created_at"`
+	Message   string    `json:"message" form:"message"`
+	Image     string    `json:"image" form:"image"`
+	Audio     string    `json:"audio" form:"audio"`
+	CreatedAt time.Time `json:"created_at" form:"created_at"`
 }
 
 type ComplaintsResponse struct {
-	UserID              uint      `json:"user_id" form:"user_id"`
-	UserFullname        string    `json:"user_fullname" form:"user_fullname"`
-	DoctorID            uint      `json:"Doctor_id" form:"Doctor_id"`
-	DoctorFullname      string    `json:"doctor_fullname" form:"doctor_fullname"`
-	Specialist          string    `json:"specialist" form:"specialist"`
-	DoctorStatus        bool      `json:"doctor_status" form:"doctor_status"`
-	DoctorTransactionID uint      `json:"doctor_transaction_id" form:"doctor_transaction"`
-	CreatedAt           time.Time `json:"created_at" form:"created_at"`
-	HealthDetails       string    `json:"health_details" form:"health_details"`
-	PatientStatus       string    `json:"patient_status" form:"patient_status"`
+	UserID              uint      `json:"user_id"`
+	UserFullname        string    `json:"user_fullname"`
+	DoctorID            uint      `json:"doctor_id"`
+	DoctorFullname      string    `json:"doctor_fullname"`
+	Specialist          string    `json:"specialist"`
+	DoctorStatus        bool      `json:"doctor_status"`
+	DoctorTransactionID uint      `json:"doctor_transaction_id"`
+	CreatedAt           time.Time `json:"created_at"`
+	HealthDetails       string    `json:"health_details"`
+	PatientStatus       string    `json:"patient_status"`
 }
 
 type UserComplaintResponse struct {
-	UserFullname        string    `json:"user_fullname" form:"user_fullname"`
-	Gender              string    `json:"gender" form:"gender"`
-	Weight              int       `json:"weight" form:"weight"`
-	CreatedAt           time.Time `json:"created_at" form:"created_at"`
-	DoctorTransactionID uint      `json:"doctor_transaction_id" form:"doctor_transaction"`
-	HealthDetails       string    `json:"health_details" form:"health_details"`
-	PatientStatus       string    `json:"patient_status" form:"patient_status"`
+	UserFullname        string    `json:"user_fullname"`
+	Gender              string    `json:"gender"`
+	Weight              int       `json:"weight"`
+	CreatedAt           time.Time `json:"created_at"`
+	DoctorTransactionID uint      `json:"transaction_id"`
+	HealthDetails       string    `json:"health_details"`
+	PatientStatus       string    `json:"patient_status"`
 }
