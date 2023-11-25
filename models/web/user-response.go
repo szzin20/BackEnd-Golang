191078b28427e6c251b4a3d1f0020bf93081b51e
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-11-25 23:10:37 +0700

    feat: update route

--- models/web/user-response.go
+++ models/web/user-response.go
@@ -23,15 +23,15 @@ type UserUpdateResponse struct {
 }
 
 type UserAllResponseByAdmin struct {
-	ID        uint   `json:"id" form:"id"`
-	Fullname  string `json:"fullname" form:"fullname"`
-	Email     string `json:"email" form:"email"`
-	Image     string `json:"image" form:"image"`
-	Gender    string `json:"gender" form:"gender"`
-	Birthdate string `json:"birthdate" form:"birthdate"`
-	BloodType string `json:"blood_type" form:"blood_type"`
-	Height    int    `json:"height" form:"height"`
-	Weight    int    `json:"weight" form:"weight"`
-	Role      string `json:"role" form:"role"`
+	ID             uint   `json:"id" form:"id"`
+	Fullname       string `json:"fullname" form:"fullname"`
+	Email          string `json:"email" form:"email"`
+	ProfilePicture string `json:"profile_picture" form:"profile_picture"`
+	Gender         string `json:"gender" form:"gender"`
+	Birthdate      string `json:"birthdate" form:"birthdate"`
+	BloodType      string `json:"blood_type" form:"blood_type"`
+	Height         int    `json:"height" form:"height"`
+	Weight         int    `json:"weight" form:"weight"`
+	Role           string `json:"role" form:"role"`
 	// DoctorTransaction []DoctorTransaction `gorm:"ForeignKey:DoctorID;references:ID"`
 }
