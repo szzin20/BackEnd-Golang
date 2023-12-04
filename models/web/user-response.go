fef2c6bf08533774e25bbc825091fef4b0a3d72f
Hanisah Fildza Annafisah <93438625+fildzaanf@users.noreply.github.com>
2023-12-04 17:44:44 +0700

    feat(chats): add roomchat doctors

--- models/web/user-response.go
+++ models/web/user-response.go
@@ -23,14 +23,20 @@ type UserUpdateResponse struct {
 }
 
 type UserAllResponseByAdmin struct {
-	ID             uint   `json:"id" form:"id"`
-	Fullname       string `json:"fullname" form:"fullname"`
-	Email          string `json:"email" form:"email"`
-	ProfilePicture string `json:"profile_picture" form:"profile_picture"`
-	Gender         string `json:"gender" form:"gender"`
-	Birthdate      string `json:"birthdate" form:"birthdate"`
-	BloodType      string `json:"blood_type" form:"blood_type"`
-	Height         int    `json:"height" form:"height"`
-	Weight         int    `json:"weight" form:"weight"`
+	ID             uint   `json:"id"`
+	Fullname       string `json:"fullname"`
+	Email          string `json:"email"`
+	ProfilePicture string `json:"profile_picture"`
+	Gender         string `json:"gender"`
+	Birthdate      string `json:"birthdate"`
+	BloodType      string `json:"blood_type"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
 	// DoctorTransaction []DoctorTransaction `gorm:"ForeignKey:DoctorID;references:ID"`
+}
+
+type UserProfileRoomchat struct {
+	ID             uint   `json:"id"`
+	Fullname       string `json:"fullname"`
+	ProfilePicture string `json:"profile_picture"`
 }
